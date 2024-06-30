7efe15a728f37622cd605071b106c86948e7036b
drashtirathod21 <drashtir9@gmail.com>
2024-06-30 14:41:06 +0530

    feat: Add user role and set default balance for user based on role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { user: 0, admin: 1 }
+  validates :name, presence: true, uniqueness: true
+
+  after_initialize :set_default_wallet_balance, if: :new_record?
+
+  private
+
+  def set_default_wallet_balance
+    if self.admin?
+      self.wallet_balance ||= 1000000
+    else
+      self.wallet_balance ||= 10000
+    end
+  end
 end
